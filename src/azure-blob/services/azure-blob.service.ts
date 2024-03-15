1e34c901a65cf149ca6b31af7b1a80be98048265
ricardocordeiro92 <ricardoaugusto.cordeiro@gmail.com>
2024-03-15 18:35:22 -0300

    feat: add generateTemporaryUrl and listFiles functions in azure-blob service

--- src/azure-blob/services/azure-blob.service.ts
+++ src/azure-blob/services/azure-blob.service.ts
@@ -1,7 +1,11 @@
-import { BlobServiceClient, BlockBlobClient } from '@azure/storage-blob';
+import {
+  BlobSASPermissions,
+  BlobServiceClient,
+  BlockBlobClient,
+} from '@azure/storage-blob';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { uuid } from 'uuidv4';
-import { UploadSuccessResponse } from '../dtos/uploadSuccessResponse';
+import { SuccessResponse } from '../dtos/successResponse';
 
 @Injectable()
 export class AzureBlobService {
@@ -24,7 +28,13 @@ export class AzureBlobService {
 
       const blobClient = containerClient.getBlockBlobClient(imageName);
 
-      return blobClient;
+      if (blobClient) {
+        return blobClient;
+      } else {
+        throw new BadRequestException(
+          `Blob client is undefined for container: ${containerName} and image: ${imageName}.`,
+        );
+      }
     } catch (error) {
       throw new BadRequestException(
         `Error getting blob client: ${error.message}.`,
@@ -35,18 +45,23 @@ export class AzureBlobService {
   async uploadFile(
     file: Express.Multer.File,
     containerName: string,
-  ): Promise<UploadSuccessResponse> {
-    const fileName = uuid() + '_' + file.originalname;
+  ): Promise<SuccessResponse> {
+    const fileName = uuid() + file.originalname;
 
     try {
       const blobClient = await this.getBlobClient(containerName, fileName);
 
       await blobClient.uploadData(file.buffer);
+      const temporaryUrl = await this.generateTemporaryUrl(
+        fileName,
+        containerName,
+      );
 
       return {
         message: 'File uploaded successfully',
         fileName: fileName,
         containerName: containerName,
+        url: temporaryUrl,
       };
     } catch (error) {
       throw new BadRequestException(error.message);
@@ -69,20 +84,78 @@ export class AzureBlobService {
     }
   }
 
-  async deleteFile(filename: string, containerName: string): Promise<string> {
+  async deleteFile(fileName: string, containerName: string): Promise<string> {
     try {
-      const blobClient = await this.getBlobClient(containerName, filename);
+      const blobClient = await this.getBlobClient(containerName, fileName);
       const result = await blobClient.deleteIfExists();
 
       if (result.succeeded) {
-        return `File ${filename} deleted successfully.`;
+        return `File ${fileName} deleted successfully.`;
       } else {
-        throw new BadRequestException(`Failed to delete file ${filename}.`);
+        throw new BadRequestException(`Failed to delete file ${fileName}.`);
       }
     } catch (error) {
       throw new BadRequestException(
-        `Failed to delete file ${filename}: ${error.message}.`,
+        `Failed to delete file ${fileName}: ${error.message}.`,
       );
+    }
+  }
+
+  async generateTemporaryUrl(
+    fileName: string,
+    containerName: string,
+  ): Promise<string> {
+    try {
+      const blobClient = await this.getBlobClient(containerName, fileName);
+
+      const startDate = new Date();
+      const expiryDate = new Date(startDate);
+      expiryDate.setMinutes(startDate.getMinutes() + 100);
+      startDate.setMinutes(startDate.getMinutes() - 100);
+
+      const sharedAccessPolicy = {
+        permissions: BlobSASPermissions.parse('r'), // Permissão de leitura
+        startsOn: startDate,
+        expiresOn: expiryDate,
+      };
+
+      const token = await blobClient.generateSasUrl(sharedAccessPolicy);
+
+      return token;
+    } catch (error) {
+      throw new BadRequestException(
+        `Failed to generate temporary URL: ${error.message}.`,
+      );
+    }
+  }
+
+  async listFiles(containerName: string): Promise<SuccessResponse[]> {
+    try {
+      const blobServiceClient = BlobServiceClient.fromConnectionString(
+        this.azureConnection,
+      );
+      const containerClient =
+        blobServiceClient.getContainerClient(containerName);
+
+      const files: SuccessResponse[] = [];
+
+      for await (const blob of containerClient.listBlobsFlat()) {
+        const temporaryUrl = await this.generateTemporaryUrl(
+          blob.name,
+          containerName,
+        );
+
+        files.push({
+          message: `File ${blob.name} listed successfully`,
+          fileName: blob.name,
+          containerName,
+          url: temporaryUrl,
+        });
+      }
+
+      return files;
+    } catch (error) {
+      throw new BadRequestException(`Failed to list files: ${error.message}.`);
     }
   }
 }
